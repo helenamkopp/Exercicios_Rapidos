72c08951e403f3d1e155df5217098f71cc62c08c
Helena Meyer Köpp <63676413+helenamkopp@users.noreply.github.com>
2020-05-11 13:42:06 -0300

    Add files via upload

---
+++ ex046.py
@@ -0,0 +1,9 @@
+from time import sleep
+for cont in range (10, -1, -1 ): # esse -1 no final significa que ele vai contar ao contrário.
+    print(cont)
+    sleep(1)
+print("BUM! BUM! POOOW! ")
+sleep(1)
+print("Feliz ano novo!")
+
+# é importante prestar atenção na identação do programa
